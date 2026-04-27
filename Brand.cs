71e60198f90d9b9ed1ff6c4f46591b81d6748669
Mohamed Alhaleeth A <121995787+Alhaleeth@users.noreply.github.com>
2026-04-27 18:04:39 +0530

    Add files via upload

---
+++ Brand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopSpeed.Web.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid Id { get; set; }  
+
+        [Required]
+        public string Name { get; set; }
+        [Display(Name="Manufacturing Year")]
+        public int ManufacturingYear { get; set; }
+        [Display(Name = "Brand Logo")]
+        public string BrandLogo { get; set; }
+    }
+}
